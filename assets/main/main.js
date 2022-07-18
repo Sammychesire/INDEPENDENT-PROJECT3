65f048480dd00f1ed7e804fb4e56f82495e014cd
bob <sammy.chesire@student.moringaschool.com>
2022-07-18 08:13:30 +0300

    hot fix repository pushing

---
+++ assets/main/main.js
@@ -0,0 +1,16 @@
+var milkShed=[510,308,486,572]
+let count=1
+var production
+
+function totalProduction(milkShed){
+for(let i=0;i<milkShed.length;i++){
+    //production=Math.sum(milkShed)
+    console.log("your production in shed "+count+ " is "  + milkShed[i])
+    count++
+}
+}
+totalProduction(milkShed);
+//production
+
+           
+           
